65ae02d037d1ab7b540b26b7778f56e0c64426b5
BPitts8019 <bradleyj.pitts@gmail.com>
2019-11-13 19:57:07 -0700

    moved App.js into components folder for better organization

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import Colors from "./components/Colors";
+
+function App() {
+   return (
+      <div className="App">
+         <h1>Welcome to the App</h1>
+      </div>
+   );
+}
+
+export default App;
