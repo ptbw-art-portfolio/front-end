86249f40e1e026edd912b75b54db6aa87da4e5c6
wombat1972 <53923403+wombat1972@users.noreply.github.com>
2019-11-14 22:13:02 -0500

    minor styling done

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -3,9 +3,41 @@ import React, { useEffect } from 'react'
 import { Link } from 'react-router-dom'
 import { Form, Field, withFormik } from 'formik'
 import * as Yup from 'yup'
+import styled from "styled-components"
 
 import axios from 'axios';
 
+
+const Card = styled.div`
+display: flex;
+width: 35%;
+margin: 0 auto;
+flex-wrap: wrap;
+flex-direction: column;
+justify-content: center;
+align-items: center;
+background-color: #11a7f7;
+border: solid 4px black;
+border-radius: 8px;
+font-weight: 600;
+font-size: 18px;
+box-shadow: 7px 7px 5px; #2F4F4F;
+ -moz-box-shadow: 7px 7px 5px #2F4F4F;
+ -webkit-box-shadow: 7px 7px 5px #2F4F4F;
+ -khtml-box-shadow: 7px 7px 7px #2F4F4F;
+ `;
+
+ const Input = styled.div`
+    width: 70%;
+    margin: 0 auto;
+    margin-bottom: 15px;
+ `;
+
+ const Button = styled.div`
+ width: 50%;
+ margin: 0 auto;
+ margin-bottom: 15px;
+`;
 
 const Login = ({ history, status }) => {
 
@@ -29,16 +61,16 @@ const Login = ({ history, status }) => {
   }, [status])
 
   return (
-    <div>
-      <h1>
-        Welcome Artist
-      </h1>
-      <div>
+     
+      <Card>
 
         {/* Start of form */}
         <Form>
           <div>
-            <div>
+          <h1>
+        Welcome Artist
+      </h1>
+            <Input>
               <Field
                 variant='outlined'
                 required
@@ -46,9 +78,10 @@ const Login = ({ history, status }) => {
                 label='Name'
                 name='name'
                 type='name'
+                placeholder='UserName'
               />
-            </div>
-            <div>
+            </Input>
+            <Input>
               <Field
                 variant='outlined'
                 required
@@ -56,19 +89,20 @@ const Login = ({ history, status }) => {
                 name='password'
                 label='Password'
                 type='password'
+                placeholder='Password'
               />
-            </div>
+            </Input>
           </div>
+          <Button>
           <button onClick={null}>
             Log In
           </button>
+          </Button>
           <div>
             <Link to='/register'>Need to register?</Link>
-            <Link to='/reset'>Forgot Password?</Link>
           </div>
         </Form>
-      </div>
-    </div>
+      </Card>
   )
 }
 
