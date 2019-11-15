23c050042ac6169fee161321991cc17a74a06862
vishalicious213 <vishalicious213@gmail.com>
2019-11-15 03:59:11 -0500

    artist gallery thumbnails use username and fullname variables from backend specs. username is unique key

--- src/components/ArtistGallery.js
+++ src/components/ArtistGallery.js
@@ -3,13 +3,13 @@ import './ArtistGallery.css';
 // import styled from 'styled-components';
 
 const artists = [
-  {name: "Leonardo Da Vinci", img: "./img/mona_lisa_by_leonardo_da_vinci.jpg"},
-  {name: "Vincent Van Gogh", img: "./img/the_starry_night_by_vincent_van_gogh.jpg"},
-  {name: "Rembrandt", img: "./img/the_night_watch_by_rembrandt.jpg"},
-  {name: "Gustav Klimt", img: "./img/the_kiss_by_gustav_klimt.jpg"},
-  {name: "Jan Van Eyck", img: "./img/the_arnolfini_portrait_by_jan_van_eyck.jpg"},
-  {name: "Johannes Vermeer", img: "./img/the_girl_with_a_pearl_earring_by_johannes_vermeer.jpg"},
-  {name: "Claude Monet", img: "./img/impression,_sunrise_by_claude_monet.jpg"},
+  {username: "leonardo", fullname: "Leonardo Da Vinci", img: "./img/mona_lisa_by_leonardo_da_vinci.jpg"},
+  {username: "vincent", fullname: "Vincent Van Gogh", img: "./img/the_starry_night_by_vincent_van_gogh.jpg"},
+  {username: "rembrandt", fullname: "Rembrandt", img: "./img/the_night_watch_by_rembrandt.jpg"},
+  {username: "gustav", fullname: "Gustav Klimt", img: "./img/the_kiss_by_gustav_klimt.jpg"},
+  {username: "jan", fullname: "Jan Van Eyck", img: "./img/the_arnolfini_portrait_by_jan_van_eyck.jpg"},
+  {username: "johannes", fullname: "Johannes Vermeer", img: "./img/the_girl_with_a_pearl_earring_by_johannes_vermeer.jpg"},
+  {username: "claude", fullname: "Claude Monet", img: "./img/impression,_sunrise_by_claude_monet.jpg"},
 ];
 
 function ArtistGallery() {
@@ -17,8 +17,8 @@ function ArtistGallery() {
     <section className="ArtistGallery">
       {artists.map(artist => {
         return (
-          <div className="ArtistGalleryThumbnail" key={artist.name}>
-            <img className="ArtistGalleryImage" src={require(`${artist.img}`)} title={`${artist.name}'s works`} alt={`${artist.name}'s works`}></img>
+          <div className="ArtistGalleryThumbnail" key={artist.username}>
+            <img className="ArtistGalleryImage" src={require(`${artist.img}`)} title={`${artist.fullname}'s works`} alt={`${artist.fullname}'s works`}></img>
             <div className="attribution">{artist.name}</div>
           </div>
         )
