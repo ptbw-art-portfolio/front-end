a10d35ac984e4787cac1e52483b3e276cafb5b92
BPitts8019 <bradleyj.pitts@gmail.com>
2019-11-14 18:23:26 -0700

    Reverting App.js back to master

--- src/components/App.js
+++ src/components/App.js
@@ -1,46 +1,15 @@
-import React, {useEffect} from 'react';
-import {connect} from "react-redux";
+import React from 'react';
 import Colors from "./Colors";
-import {axiosWithAuth as axios} from "../utils/axiosWithAuth";
 
 //action creators
-import { login } from "../store/auth/useAuthActions";
+import {} from "../store/auth/useAuthActions";
 
-function App({token, error, login}) {
-   useEffect(() => {
-      //check for a stored token
-      console.log(`Token: ${token}`);
-      login();
-      if (!token) {
-         token = localStorage.getItem("token");
-      }
-      
-      axios(token)
-         .then()
-
-
-
-   }, []);
-
+function App() {
    return (
       <div className="App">
          <h1>Welcome to the App</h1>
-
-         {token && <div>User logged in</div>}
-         {error && <div>{}</div>}
       </div>
    );
 }
 
-const mapStateToProps = state => {
-   return {
-      token: state.auth.token,
-      error: state.auth.error
-   };
-};
-
-const mapDispatchToProps = {
-   login
-};
-
-export default connect(mapStateToProps, mapDispatchToProps)(App);
+export default App;
