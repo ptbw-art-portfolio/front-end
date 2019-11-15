1f0effc976e69d253ef5b6a7b6b5d7059f876238
vishalicious213 <vishalicious213@gmail.com>
2019-11-15 01:42:12 -0500

    moving all files and updates from personal repo to org repo

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,35 @@
 import React from 'react';
 import Colors from "./Colors";
+ // import { Route } from 'react-router-dom';
+import './App.css';
+import ArtistGallery from "./ArtistGallery"
+import NavBar from './NavBar';
+// import styled from 'styled-components';
 
 //action creators
 import {} from "../store/auth/useAuthActions";
 
 function App() {
    return (
-      <div className="App">
-         <h1>Welcome to the App</h1>
-      </div>
+     <div className="mainProgram">
+       <section className="top">
+         <NavBar />
+         {/* <Route path='/upload' component={} /> */}
+         {/* <Route path='/sign-in' component={} /> */}
+ 
+         <div className="banner">
+           <h1>Art Portfolio</h1>
+         </div>
+ 
+         <form className="artistSearchForm">
+           <input className="artistSearchBox" type="text" name="search" placeholder="&#x1f50d; Search artists..." />
+         </form>
+       </section>
+ 
+       <ArtistGallery />
+     </div>
    );
-}
-
-export default App;
+ }
+ 
+ export default App;
+ 
