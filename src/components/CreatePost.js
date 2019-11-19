d2487308fbbec61caf2137bd6fa9a91777fdba10
vishalicious213 <vishalicious213@gmail.com>
2019-11-18 23:43:57 -0500

    CreatePost set up to accept input from user with Formik

--- src/components/CreatePost.js
+++ src/components/CreatePost.js
@@ -1,23 +1,26 @@
 import React from 'react';
 // import styled from 'styled-components';
+import { Formik } from 'formik';
 import './CreatePost.css';
 
 function CreatePost() {
     return (
-        <div className="CreatePost">
-            <form className="NewPost">
-            <input type="text" className="TextField" name="title" placeholder="Title" />
-            <input type="text" className="TextField" name="date" placeholder="Date" />
-            <input type="text" className="TextField" name="medium" placeholder="Medium" />
-            <input type="text" className="TextField" name="link" placeholder="Image Link" />
-            <input type="text" className="TextField" name="details" placeholder="Description" />
-            <div className="buttonContainer">
-                <button type="button">Add</button>
-                <button type="button">Cancel</button>
-            </div>
-            </form>
-
-        </div>
+        <Formik initialValues={{title: "", date: "", medium: "", link: "", details: ""}}>
+            {({values, errors, touched, handleChange, handleBlur}) => (
+                <form className="CreatePost">
+                    {/* {JSON.stringify(values)} */}
+                    <input type="text" className="TextField" name="title" placeholder="Title" onChange={handleChange} onBlur={handleBlur} value={values.title} />
+                    <input type="text" className="TextField" name="date" placeholder="Date" onChange={handleChange} onBlur={handleBlur} value={values.date} />
+                    <input type="text" className="TextField" name="medium" placeholder="Medium" onChange={handleChange} onBlur={handleBlur} value={values.medium} />
+                    <input type="text" className="TextField" name="link" placeholder="Image Link" onChange={handleChange} onBlur={handleBlur} value={values.link} />
+                    <input type="text" className="TextField" name="details" placeholder="Description" onChange={handleChange} onBlur={handleBlur} value={values.details} />
+                    <div className="buttonContainer">
+                        <button type="button">Add</button>
+                        <button type="button">Cancel</button>
+                    </div>
+                </form>
+            )}
+        </Formik>
     );
 }
 
