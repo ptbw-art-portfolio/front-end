6ab8f56d5f0e74c4b591e2449b303dddd9213e49
BPitts8019 <bradleyj.pitts@gmail.com>
2019-11-19 20:33:09 -0700

    Updating details redux store; incomplete

--- src/store/details/reducer.js
+++ src/store/details/reducer.js
@@ -1,27 +1,32 @@
 import { FETCH_DETAILS_START } from "./actionTypes";
 
+const NO_ERROR = null;
+const NO_DETAILS = {
+   id: -1,
+   title: "",
+   medium: "",
+   image_url: "",
+   description: "",
+   likes: "",
+   created_at: "",
+   updated_at: "",
+   user_id: "",
+   artist: ""
+};
 const INITIAL_STATE = {
    isLoading: false,
-   error: null,
-   imgDetails: {
-      id: -1,
-      title: "",
-      medium: "",
-      image_url: "",
-      description: "",
-      likes: "",
-      created_at: "",
-      updated_at: "",
-      user_id: "",
-      artist: ""
-   },
+   error: NO_ERROR,
+   imgDetails: NO_DETAILS
 };
 
 export default (state = INITIAL_STATE, action) => {
    switch (action.type) {
       case FETCH_DETAILS_START:
          return {
-            
+            ...state,
+            isLoading: true,
+            error: NO_ERROR,
+            imgDetails: NO_DETAILS
          }
       default:
          return state;
--- src/store/details/useActions.js
+++ src/store/details/useActions.js
@@ -1,29 +1,31 @@
 import {axiosWithAuth as axios, setToken} from "../../utils/axiosWithAuth";
-import { FETCH_DETAILS_START } from "./actionTypes";
+import { FETCH_DETAILS_START, FETCH_DETAILS_SUCCESS } from "./actionTypes";
 
 export function getImageDetails (id) {
    return dispatch => {
-      console.log("Getting Image Details...");
-      dispatch(FETCH_DETAILS_START);
+      dispatch({type: FETCH_DETAILS_START});
+      console.log(`Retrieveing details for image ID-${id}...`);
       
+      //GET post by ID
+      axios().get(`/posts/${id}`)
+         .then(api_resp => {
+            // dispatch({type: FETCH_DETAILS_SUCCESS, payload: api_resp});
+            console.log(api_resp);
+            // return new Promise((resolve, reject) => {
+            //    if (!api_resp.data.data) {
+            //       console.log("Failed to retrieve post!!");
+            //       reject(api_resp.data);
+            //    } else {
+            //       console.log("Success!");
+            //       resolve(api_resp.data.data[0]);
+            //    }
+            // });
+         })
    };
    // return dispatch => {
-   //    dispatch(FETCH_DETAILS_START);
-   //    console.log(`Retrieveing details for image ID-${id}...`);
+ 
 
-   //    //GET post by ID
-   //    axios().get(`/posts/${id}`)
-   //       .then(api_resp => {
-   //          return new Promise((resolve, reject) => {
-   //             if (!api_resp.data.data) {
-   //                console.log("Failed to retrieve post!!");
-   //                reject(api_resp.data);
-   //             } else {
-   //                console.log("Success!");
-   //                resolve(api_resp.data.data[0]);
-   //             }
-   //          });
-   //       })
+
    //       .then(post_data => {
    //          newDetails = {
    //             ...newDetails,
