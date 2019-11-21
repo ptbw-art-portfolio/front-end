2396c595231c9e6970ac1a21f4ab8098f98a95fc
BPitts8019 <bradleyj.pitts@gmail.com>
2019-11-21 15:23:19 -0700

    Added better styling

--- src/components/ImageDetails.jsx
+++ src/components/ImageDetails.jsx
@@ -2,24 +2,55 @@ import React, { useEffect } from "react"
 import { connect } from "react-redux";
 import { getImageDetails } from "../store/details/useActions";
 import { getToken } from "../utils/axiosWithAuth";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faUserCircle} from "@fortawesome/free-solid-svg-icons";
 
 //*** Component Styles ***//
 import styled from "styled-components";
+import {colors, MAX_WIDTH} from "./style-utils/variables";
 import { autoScale, customLayout } from "./style-utils/mixins";
 
-const Wrapper = styled.div`
-   background-color: dodgerblue;
-   
-   img {
-      ${autoScale}
+const StyledArticle = styled.article`
+   .img-wrapper {
+      width: 100%;
+      box-shadow: inset 0 0 20px 4px ${colors.darkText};
+      ${customLayout("center")}
+      
+      @media only screen and (min-width: 768px) {
+         box-shadow: inset 0 0 20px 4px ${colors.darkText};
+         padding: 30px;
+      }
+
+      img {
+         ${autoScale}
+         max-height: 480px;
+      }
    }
 
    .card-wrapper {
-      ${customLayout("space-between", "flex-end")};
+      width: 100%;
+      max-width: ${MAX_WIDTH};
+      margin: 0 auto;
+      padding: 1rem;
+      ${customLayout("flex-start", "flex-start", "column")};
 
       .card-info {
-         width: 70%;
-         background-color: red;
+         & > * {
+            margin-bottom: 1rem;
+         }
+
+         &:last-child {
+            margin: auto auto;
+         }
+
+         .artist {
+            height: 4rem;
+            svg {
+               margin-right: 2rem;
+            }
+
+            ${customLayout("flex-start", "center")}
+         }
       }
 
       .user-controls {
@@ -29,17 +60,6 @@ const Wrapper = styled.div`
       }
    }
 `;
-
-//Connect to Redux store
-const mapStateToProps = state => {
-   return {
-      ...state.details
-   };
-};
-
-const mapDispatchToProps = {
-   getImageDetails
-};
 
 function ImageDetails({ isLoading, error, imgDetails, match: { params: { id } }, getImageDetails }) {
    //Get image details on load or if match changes somehow
@@ -56,11 +76,16 @@ function ImageDetails({ isLoading, error, imgDetails, match: { params: { id } },
    } else {
       innerHTML = (
          <>
-            <img src="https://via.placeholder.com/1600x900" alt="A dove" />
+            <div className="img-wrapper">
+               <img src="https://via.placeholder.com/1600x900" alt="A dove" />
+            </div>
 
             <div className="card-wrapper">
                <div className="card-info">
-                  <h2 className="artist">{imgDetails.artist}</h2>
+                  <div className="artist">
+                     <FontAwesomeIcon icon={faUserCircle} size="3x" />
+                     <h2>{imgDetails.artist}</h2>
+                  </div>
                   <h2 className="title">{imgDetails.title}</h2>
                   <h4 className="art-medium">{imgDetails.medium}</h4>
                </div>
@@ -74,14 +99,21 @@ function ImageDetails({ isLoading, error, imgDetails, match: { params: { id } },
                      )
                      : null
                }
+               <p className="description">{imgDetails.description}</p>
             </div>
-            <p className="description">{imgDetails.description}</p>
          </>
       );
    }
 
-   return <Wrapper><article className="content">{innerHTML}</article></Wrapper>;
+   return <StyledArticle>{innerHTML}</StyledArticle>;
 }
+
+//Connect to Redux store
+const mapStateToProps = state => {
+   return {
+      ...state.details
+   };
+};
 
 export default connect(mapStateToProps, {getImageDetails})(ImageDetails);
 
