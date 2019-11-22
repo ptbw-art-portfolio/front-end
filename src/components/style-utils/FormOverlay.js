69d3e6dd602cb7573ee5b6da109602a37f57eb42
BPitts8019 <bradleyj.pitts@gmail.com>
2019-11-22 14:41:56 -0700

    Extracted FormOverlay from Login.js into its own component

---
+++ src/components/style-utils/FormOverlay.js
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+// import {} from "./variables";
+// import {} from "./mixins";
+
+export default styled.div`
+   background: #f26656b2;
+   background: linear-gradient(74deg, #f26656b2 0%, #4b559cb2 100%);
+   z-index: 1;
+   width: 100%;
+   height: 100%;
+   position: fixed;
+   top: 0;
+   left: 0;
+   overflow: auto;
+   display: flex;
+   justify-content: center;
+   align-items: center;
+`;
