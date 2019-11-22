94d6d1c3f1ef76b9159adcb606c0037fe53513dc
vishalicious213 <vishalicious213@gmail.com>
2019-11-21 19:06:29 -0500

    upgraded CreatePost from CSS to Styled Components

--- src/components/CreatePost.js
+++ src/components/CreatePost.js
@@ -1,9 +1,57 @@
 import React from 'react';
-// import styled from 'styled-components';
+import styled from 'styled-components';
 import { Formik } from 'formik';
 import * as Yup from 'yup';
-import './CreatePost.css';
+// import './CreatePost.css';
+import { axiosWithAuth as axios } from '../utils/axiosWithAuth';
 
+const Wrapper = styled.div`
+    .CreatePost {
+        border-radius: .75rem;
+        width: 32rem;
+        padding-top: 3rem;
+        padding-bottom: 5rem;
+        text-align: center;
+        background-color: #fefefe;
+        border: 1px solid #888;
+
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+    }
+
+    .fieldContainer {
+    width: 100%;
+    }
+
+    .TextField {
+        width: 80%;
+        margin-top: 1rem;
+    }
+
+    .buttonContainer {
+        width: 80%;
+        display: flex;
+        justify-content: space-between;
+        border-radius: .35rem;
+        margin-top: 1rem;
+    }
+
+    button {
+        width: 10rem;
+    }
+
+    .form-message {
+        font-size: .75rem;
+        padding-top: .25rem;
+    }
+    .form-message.valid {
+        color: limegreen;
+    }
+    .form-message.invalid {
+        color: crimson;
+    }
+`
 const validationSchema = Yup.object().shape({
     title: Yup.string()
         .min(1, "Must have at least one letter")
@@ -39,69 +87,69 @@ const Error = ({ touched, message}) => {
 
 function CreatePost(props) {
     return (
-        <Formik 
-            initialValues={{title: "", date: "", medium: "", link: "", details: ""}}
-            validationSchema={validationSchema}
-            onSubmit={(values, {setSubmitting, resetForm}) => {
-                setSubmitting(true);
-                alert(JSON.stringify(values, null, 2));
-                // remove alert and place axios call or save function call here
-                resetForm();
-                setSubmitting(false);
-            }}>
+        <Wrapper>
+            <Formik 
+                initialValues={{title: "", date: "", medium: "", link: "", details: ""}}
+                validationSchema={validationSchema}
+                onSubmit={(values, {setSubmitting, resetForm}) => {
+                    setSubmitting(true);
+                    alert(JSON.stringify(values, null, 2));
+                    // remove alert and place axios call or save function call here
+                    resetForm();
+                    setSubmitting(false);
+                }}>
 
-            {({values, errors, touched, handleChange, handleBlur, handleSubmit, isSubmitting}) => (
-                <form className="CreatePost" onSubmit={handleSubmit}>
-                    {/* {JSON.stringify(values)} */}
-                    <section className="fieldContainer">
-                        <div className="inputContainer">
-                            <input type="text" name="title" placeholder="Title (required)"
-                                className={`TextField ${touched.title && errors.title ? "has-error" : null}`}
-                                onChange={handleChange} onBlur={handleBlur} value={values.title} />
-                            <Error touched={touched.title} message={errors.title} />
+                {({values, errors, touched, handleChange, handleBlur, handleSubmit, isSubmitting}) => (
+                    <form className="CreatePost" onSubmit={handleSubmit}>
+                        {/* {JSON.stringify(values)} */}
+                        <section className="fieldContainer">
+                            <div className="inputContainer">
+                                <input type="text" name="title" placeholder="Title (required)"
+                                    className={`TextField ${touched.title && errors.title ? "has-error" : null}`}
+                                    onChange={handleChange} onBlur={handleBlur} value={values.title} />
+                                <Error touched={touched.title} message={errors.title} />
+                            </div>
+
+                            <div className="inputContainer">
+                                <input type="date" name="date" placeholder="Date" 
+                                    className={`TextField ${touched.date && errors.date ? "has-error" : null}`}
+                                    onChange={handleChange} onBlur={handleBlur} value={values.date} />
+                                <Error touched={touched.date} message={errors.date} />
+                            </div>
+
+                            <div className="inputContainer">
+                                <input type="text" name="medium" placeholder="Medium" 
+                                    className={`TextField ${touched.medium && errors.medium ? "has-error" : null}`}
+                                    onChange={handleChange} onBlur={handleBlur} value={values.medium} />
+                                <Error touched={touched.medium} message={errors.medium} />
+                            </div>
+
+                            <div className="inputContainer">
+                                <input type="url" name="link" placeholder="Image Link (required)" 
+                                    className={`TextField ${touched.link && errors.link ? "has-error" : null}`}
+                                    onChange={handleChange} onBlur={handleBlur} value={values.link} />
+                                <Error touched={touched.link} message={errors.link} />
+                            </div>
+
+                            <div className="inputContainer">
+                                <textarea rows="5" cols="30" name="details" placeholder="Description (up to 150 chars)" maxLength="150"
+                                    className={`TextField ${touched.details && errors.details ? "has-error" : null}`}
+                                    onChange={handleChange} onBlur={handleBlur} value={values.details}>
+                                </textarea>
+                                <Error touched={touched.details} message={errors.details} />
+                            </div>
+                        </section>
+
+                        <div className="buttonContainer">
+                            <button type="submit" disabled={isSubmitting}>Add</button>
+                            <button type="button" id="cancel"
+                                onClick={props.onClose}>Cancel</button>
+
                         </div>
-
-                        <div className="inputContainer">
-                            <input type="date" name="date" placeholder="Date" 
-                                className={`TextField ${touched.date && errors.date ? "has-error" : null}`}
-                                onChange={handleChange} onBlur={handleBlur} value={values.date} />
-                            <Error touched={touched.date} message={errors.date} />
-                        </div>
-
-                        <div className="inputContainer">
-                            <input type="text" name="medium" placeholder="Medium" 
-                                className={`TextField ${touched.medium && errors.medium ? "has-error" : null}`}
-                                onChange={handleChange} onBlur={handleBlur} value={values.medium} />
-                            <Error touched={touched.medium} message={errors.medium} />
-                        </div>
-
-                        <div className="inputContainer">
-                            <input type="url" name="link" placeholder="Image Link (required)" 
-                                className={`TextField ${touched.link && errors.link ? "has-error" : null}`}
-                                onChange={handleChange} onBlur={handleBlur} value={values.link} />
-                            <Error touched={touched.link} message={errors.link} />
-                        </div>
-
-                        <div className="inputContainer">
-                            <textarea rows="5" cols="30" name="details" placeholder="Description (up to 150 chars)" maxLength="150"
-                                className={`TextField ${touched.details && errors.details ? "has-error" : null}`}
-                                onChange={handleChange} onBlur={handleBlur} value={values.details}>
-                            </textarea>
-                            <Error touched={touched.details} message={errors.details} />
-                        </div>
-                    </section>
-
-                    <div className="buttonContainer">
-                        <button type="submit" disabled={isSubmitting}>Add</button>
-                        <button type="button" id="cancel"
-                            onClick={props.onClose}>Cancel</button>
-
-                    </div>
-                </form>
-            )}
-
-
-        </Formik>
+                    </form>
+                )}
+            </Formik>
+        </Wrapper>
     );
 }
 
