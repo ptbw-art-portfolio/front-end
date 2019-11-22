f87c78c54f51976fba3117ce0d69aa8ee35f97e1
BPitts8019 <bradleyj.pitts@gmail.com>
2019-11-22 15:55:16 -0700

    Updated colors and added a cool gradient

--- src/components/style-utils/FormOverlay.js
+++ src/components/style-utils/FormOverlay.js
@@ -1,10 +1,16 @@
 import styled from "styled-components";
-// import {} from "./variables";
+import {colors} from "./variables";
 // import {} from "./mixins";
 
 export default styled.div`
-   background: #f26656b2;
-   background: linear-gradient(74deg, #f26656b2 0%, #4b559cb2 100%);
+   background: ${colors.overlayBackground[1]};
+   background: radial-gradient(
+      circle, 
+      ${colors.overlayBackground[2]} 0%, 
+      ${colors.overlayBackground[1]} 65%, 
+      ${colors.overlayBackground[1]} 95%, 
+      ${colors.overlayBackground[0]} 100%
+   );
    z-index: 1;
    width: 100%;
    height: 100%;
