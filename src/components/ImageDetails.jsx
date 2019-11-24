8128dd15ce931143efe92cacfd7ad665c4c16d26
BPitts8019 <bradleyj.pitts@gmail.com>
2019-11-23 20:38:16 -0700

    Updated user controls and styles; created a Spinner component

--- src/components/ImageDetails.jsx
+++ src/components/ImageDetails.jsx
@@ -2,8 +2,9 @@ import React, { useEffect } from "react"
 import { connect } from "react-redux";
 import { getImageDetails } from "../store/details/useActions";
 import { getToken } from "../utils/axiosWithAuth";
+import Spinner from "./Spinner";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import {faUserCircle} from "@fortawesome/free-solid-svg-icons";
+import {faUserCircle, faEdit, faTrashAlt} from "@fortawesome/free-solid-svg-icons";
 
 //*** Component Styles ***//
 import styled from "styled-components";
@@ -12,8 +13,9 @@ import { autoScale, customLayout } from "./style-utils/mixins";
 
 const StyledArticle = styled.article`
    .img-wrapper {
-      width: 100%;
       box-shadow: inset 0 0 20px 4px ${colors.darkText};
+      margin-bottom: 3rem;
+      width: 100%;
       ${customLayout("center")}
       
       @media only screen and (min-width: 768px) {
@@ -35,6 +37,9 @@ const StyledArticle = styled.article`
       ${customLayout("flex-start", "flex-start", "column")};
 
       .card-info {
+         width: 100%;
+         max-width: ${MAX_WIDTH};
+
          & > * {
             margin-bottom: 1rem;
          }
@@ -43,20 +48,21 @@ const StyledArticle = styled.article`
             margin: auto auto;
          }
 
-         .artist {
-            height: 4rem;
-            svg {
-               margin-right: 2rem;
+         .artist-wrapper {
+            ${customLayout("space-between")}
+
+            .artist {
+               ${customLayout("flex-start")}
+               svg {
+                  margin-right: 1rem;
+               }
             }
 
-            ${customLayout("flex-start", "center")}
+            .user-controls {
+               width: 5rem;
+               ${customLayout("space-between")}
+            }
          }
-      }
-
-      .user-controls {
-         background-color: yellow;
-         height: 100%;
-         ${customLayout("flex-start")};
       }
    }
 `;
@@ -66,10 +72,12 @@ function ImageDetails({ isLoading, error, imgDetails, match: { params: { id } },
    useEffect(() => getImageDetails(id), []);
 
    const isLoggedIn = (getToken() !== null);
+   // const isLoggedIn = (true);
    let innerHTML;
 
    if (isLoading) {
-      innerHTML = <p className="spinner">Loading...</p>;
+   // if (true) {
+      innerHTML = <Spinner />
    } else if (error) {
       const message = `Server Error: ${error.data.message} ${error.status}`;
       innerHTML = <p className="error">{message}</p>
@@ -82,23 +90,20 @@ function ImageDetails({ isLoading, error, imgDetails, match: { params: { id } },
 
             <div className="card-wrapper">
                <div className="card-info">
-                  <div className="artist">
-                     <FontAwesomeIcon icon={faUserCircle} size="3x" />
-                     <h2>{imgDetails.artist}</h2>
+                  <div className="artist-wrapper">
+                     <div className="artist">
+                        <FontAwesomeIcon icon={faUserCircle} size="3x" />
+                        <h2>{imgDetails.artist}</h2>
+                     </div>
+                     {(isLoggedIn) && 
+                     <div className="user-controls">
+                        <FontAwesomeIcon title="Edit Post" icon={faEdit} size="2x" />
+                        <FontAwesomeIcon title="Delete Post" icon={faTrashAlt} size="2x" />
+                     </div>}
                   </div>
                   <h2 className="title">{imgDetails.title}</h2>
                   <h4 className="art-medium">{imgDetails.medium}</h4>
                </div>
-               {
-                  (isLoggedIn)
-                     ? (
-                        <div className="user-controls">
-                           <p>Edit</p>
-                           <p>Delete</p>
-                        </div>
-                     )
-                     : null
-               }
                <p className="description">{imgDetails.description}</p>
             </div>
          </>
@@ -108,40 +113,4 @@ function ImageDetails({ isLoading, error, imgDetails, match: { params: { id } },
    return <StyledArticle>{innerHTML}</StyledArticle>;
 }
 
-//Connect to Redux store
-const mapStateToProps = state => {
-   return {
-      ...state.details
-   };
-};
-
-export default connect(mapStateToProps, {getImageDetails})(ImageDetails);
-
-
-//GET /posts/:id
-//response
-/*
-{
-   "data": [
-      {
-      "id": 3,
-      "title": "title3",
-      "medium": "paint",
-      "image_url": "someURLsdkfjl",
-      "description": "Vivamus suscipit tortor eget felis porttitor volutpat. Curabitur non nulla sit amet nisl tempus convallis quis ac lectus. Nulla quis lorem ut libero malesuada feugiat. Nulla porttitor accumsan tincidunt. Quisque velit nisi, pretium ut lacinia in, elementum id enim.",
-      "likes": 0,
-      "created_at": "2019-11-15T02:55:31.235Z",
-      "updated_at": "2019-11-15T02:55:31.235Z",
-      "user_id": 1
-      }
-   ]
-}
-*/
-
-//error
-/*
-{
-   "name": "JsonWebTokenError",
-   "message": "invalid signature"
-}
-*/
+export default connect(state => ({...state.details}), {getImageDetails})(ImageDetails);
